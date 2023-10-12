a3b893bd3ac78c06e233eba4bbaed6acc2f145c3
Mario Nunes <marionunes_azevedo@hotmail.com>
2023-10-12 00:10:06 -0300

    teste sgbd

--- protegido/scripts/scripts.js
+++ protegido/scripts/scripts.js
@@ -10,7 +10,7 @@ function cdmenu(){
     window.location.href = '/menuSGE.html'
 }
 
-const endpoint = "https://exemplo.com/api/livros"; // Alterar para a URL correta
+const endpoint = "https://129.146.68.51/sge/livro"; // Alterar para a URL correta
 const botaoCadastrar = document.getElementById('submitButton'); 
 const formulario = document.getElementById('formulario'); 
 
